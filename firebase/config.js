8e4cc3f9968aeeab1fe9b56ab224d55733bd292e
Alan <istar0me@gmail.com>
2017-12-27 14:57:42 +0800

    add navbar

---
+++ firebase/config.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/4.8.1/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAAhKCuPWlHfseOvKlkpvKARtfPrb_j7Xg",
+    authDomain: "expensemanager-3bbf7.firebaseapp.com",
+    databaseURL: "https://expensemanager-3bbf7.firebaseio.com",
+    projectId: "expensemanager-3bbf7",
+    storageBucket: "expensemanager-3bbf7.appspot.com",
+    messagingSenderId: "1057101953435"
+  };
+  firebase.initializeApp(config);
+</script>
